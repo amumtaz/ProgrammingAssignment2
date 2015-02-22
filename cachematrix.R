40c2e2db284a8a627a8fac4ea37cc81ed1de8614
Atif Mumtaz <amumtaz@computer.org>
2015-02-22 10:55:21 +0500

    Code with Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,28 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
+
+## This function sets the default values for matrix (x) and its inverse (ix)
+## r = rows
+## c = columns
+## The default for ix is NULL matrix
 setVals <- function(a,r,c){
     x <<- a
     ix <<- matrix(,r,c)
 }
 
+## This function returns the value of matrix (x) whatever it is last set to
 getVals <- function() {
     x
 #    print(x)
 }
 
+
+## This function calculates the inverse of a given matrix(a) and returns (ix) as its inverse
 setInv <- function(a){
     ix <<- solve(a)
 #    print(ix)
     return(ix)
 }
 
+## The function simply retrieves the last set value of inverse matrix (ix)
 getInv <- function() {
     ix
 #    print(ix)
 }
+
+# The function sets up values for the given matrix (x) and a NULL for its inverse (ix). 
+# To set values it calls setVals function to which it passes the following:
+## given matrix (x), and rows (r) and columns (c)
 
 makeMatrix <- function(x,r,c) {
 #    print(x) 
@@ -31,17 +43,21 @@ makeMatrix <- function(x,r,c) {
 #    getVals()
 }
 
+## This function gets the value of given matrix (x) and calculates its inverse
+## To calculate the inverse, it calls function setVals and passes given matrix to it.
+
 makeSolve <- function(x){
     ix <- getInv()
     if(is.null(ix)) {
         message("getting cached data")
         return(ix)
     }
-    r <- getVals()
-    rr <- setInv(r)
-    rr
+    r <- getVals() ## Get the value of the matrix that needs to be inversed
+    rr <- setInv(r) ## Do the inverse of given matrix
+    rr 
 }
 
+## Test cases to see if it solves the inverse for a given matrix (mat)
 #mat <- matrix(13:16, 2,2)
 #makeMatrix(mat,2,2)
 #makeSolve(mat)
